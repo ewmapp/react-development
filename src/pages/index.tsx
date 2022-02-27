dca1d0e2c16f824a8ecdedf429c1ae66438fd7b3
Edy Cruz <edmar@startbsb.com.br>
2022-02-27 01:51:48 -0300

    Update tests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,33 +4,29 @@ import Head from 'next/head'
 import { Box } from '../../packages/react/components/box/Box'
 import { Container } from '../../packages/react/components/container/Container'
 import { Button } from '../../packages/react/components/button/Button'
-import { ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons'
-
-/* const icone = () => {
-  return (
-    <svg
-      width="15"
-      height="15"
-      viewBox="0 0 15 15"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
-        fill="currentColor"
-        fillRule="evenodd"
-        clipRule="evenodd"
-      ></path>
-    </svg>
-  )
-} */
+import { IconButton } from '../../packages/react/components/iconButton/IconButton'
+import { SunIcon, PersonIcon } from '@radix-ui/react-icons'
+import { useTheme } from 'next-themes'
 
 export default function Home() {
+  const { theme, setTheme } = useTheme()
   return (
     <Box>
       <Head>
         <title>My page title</title>
       </Head>
+
+      <Container css={{ padding: 50 }}>
+        <IconButton
+          variant="ghost"
+          size="sm"
+          onClick={() =>
+            theme === 'dark' ? setTheme('light') : setTheme('dark')
+          }
+        >
+          <SunIcon />
+        </IconButton>
+      </Container>
 
       <Container>
         <h3>Theme Buttons</h3>
@@ -41,8 +37,8 @@ export default function Home() {
         css={{
           display: 'flex',
           flexDirection: 'row',
-          justifyContent: 'space-around',
           padding: '10px 20px',
+          '& > * + *': { marginLeft: '10px' },
         }}
       >
         <Button variant="solid">SOLID</Button>
@@ -72,8 +68,8 @@ export default function Home() {
         css={{
           display: 'flex',
           flexDirection: 'row',
-          justifyContent: 'space-around',
           padding: '10px 20px',
+          '& > * + *': { marginLeft: '10px' },
         }}
       >
         <Button variant="solid" outlined>
@@ -101,6 +97,53 @@ export default function Home() {
       </Container>
 
       <Container css={{ marginTop: 40 }}>
+        <h3>Theme Buttons Icons</h3>
+        <hr />
+      </Container>
+
+      <Container
+        css={{
+          display: 'flex',
+          flexDirection: 'row',
+          padding: '10px 20px',
+          '& > * + *': { marginLeft: '10px' },
+        }}
+      >
+        <IconButton size="lg">
+          <PersonIcon />
+        </IconButton>
+
+        <IconButton size="md">
+          <PersonIcon />
+        </IconButton>
+
+        <IconButton size="sm">
+          <PersonIcon />
+        </IconButton>
+      </Container>
+
+      <Container
+        css={{
+          display: 'flex',
+          flexDirection: 'row',
+          padding: '10px 20px',
+          '& > * + *': { marginLeft: '10px' },
+        }}
+      >
+        <IconButton size="lg" outlined>
+          <PersonIcon />
+        </IconButton>
+
+        <IconButton size="md" outlined>
+          <PersonIcon />
+        </IconButton>
+
+        <IconButton size="sm" outlined>
+          <PersonIcon />
+        </IconButton>
+      </Container>
+
+      <Container css={{ marginTop: 40 }}>
         <h3>Default Buttons</h3>
         <hr />
       </Container>
@@ -109,8 +152,8 @@ export default function Home() {
         css={{
           display: 'flex',
           flexDirection: 'row',
-          justifyContent: 'space-around',
           padding: '10px 20px',
+          '& > * + *': { marginLeft: '10px' },
         }}
       >
         <Button variant="light">LIGHT</Button>
@@ -129,8 +172,28 @@ export default function Home() {
         css={{
           display: 'flex',
           flexDirection: 'row',
-          justifyContent: 'space-around',
           padding: '10px 20px',
+          '& > * + *': { marginLeft: '10px' },
+        }}
+      >
+        <Button variant="dark">DARK</Button>
+        <Button variant="dark" disabled>
+          DARK
+        </Button>
+        <Button variant="dark" outlined>
+          DARK
+        </Button>
+        <Button variant="dark" outlined disabled>
+          DARK
+        </Button>
+      </Container>
+
+      <Container
+        css={{
+          display: 'flex',
+          flexDirection: 'row',
+          padding: '10px 20px',
+          '& > * + *': { marginLeft: '10px' },
         }}
       >
         <Button variant="success">SUCCESS</Button>
@@ -149,8 +212,8 @@ export default function Home() {
         css={{
           display: 'flex',
           flexDirection: 'row',
-          justifyContent: 'space-around',
           padding: '10px 20px',
+          '& > * + *': { marginLeft: '10px' },
         }}
       >
         <Button variant="danger">DANGER</Button>
@@ -163,6 +226,54 @@ export default function Home() {
         <Button variant="danger" outlined disabled>
           DANGER
         </Button>
+      </Container>
+
+      <Container css={{ marginTop: 40 }}>
+        <h3>Default Buttons Icons</h3>
+        <hr />
+      </Container>
+
+      <Container
+        css={{
+          display: 'flex',
+          flexDirection: 'row',
+          padding: '10px 20px',
+          '& > * + *': { marginLeft: '10px' },
+        }}
+      >
+        <IconButton size="lg" variant="soft">
+          <PersonIcon />
+        </IconButton>
+
+        <IconButton size="md" variant="soft">
+          <PersonIcon />
+        </IconButton>
+
+        <IconButton size="sm" variant="soft">
+          <PersonIcon />
+        </IconButton>
+      </Container>
+
+      <Container
+        css={{
+          display: 'flex',
+          flexDirection: 'row',
+          padding: '10px 20px',
+          marginBottom: 100,
+          '& > * + *': { marginLeft: '10px' },
+        }}
+      >
+        <IconButton size="lg" variant="ghost">
+          <PersonIcon />
+        </IconButton>
+
+        <IconButton size="md" variant="ghost">
+          <PersonIcon />
+        </IconButton>
+
+        <IconButton size="sm" variant="ghost">
+          <PersonIcon />
+        </IconButton>
       </Container>
     </Box>
   )
